24043f77a2fe34f7d467cc2258a9718c20cfb042
bagusuntoro <untorobagus65@gmail.com>
2024-09-10 21:02:25 +0700

    update

--- src/components/general/Footer.vue
+++ src/components/general/Footer.vue
@@ -3,7 +3,7 @@
   <footer class="sticky-footer customNav bg-primary">
     <div class="container my-auto">
       <div class="copyright text-center my-auto">
-        <strong>Copyright &copy; SBLF 2024</strong>
+        <strong>Copyright &copy; Biro Jasa AMITIE 2024</strong>
       </div>
     </div>
   </footer>
